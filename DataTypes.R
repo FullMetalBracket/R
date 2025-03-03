15626d88d0dcbc2d3eaf5f2131105f13ef470093
Alex Pegorini <alex_pegorini@hotmail.com>
2025-03-03 14:05:56 +0000

    datatypes and vectors

---
+++ DataTypes.R
@@ -0,0 +1,29 @@
+#Data Types
+products <- 'apple'
+print(products)
+
+assign('products','orange') # use assignment function
+products
+
+# assigning in the following way is not recommended
+'apple' -> products
+products
+
+'apple' -> products
+products = 'blueberry'
+products
+
+#operators
+2 > 5 | 5 > 1 
+
+#Data Types
+quantity <- 2
+price <- 2.3
+
+typeof(quantity)
+is.numeric(quantity)
+
+# Booleans
+discount <- FALSE
+discount <- F
+F
