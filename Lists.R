7fccc9975d8a77dee748b689224925e49676a6d9
Alex Pegorini <alex_pegorini@hotmail.com>
2025-03-09 22:05:24 +0000

    Lists

---
+++ Lists.R
@@ -0,0 +1,16 @@
+quantity_store_A <- c(7, 5, 3, 6, 9, 4)
+quantity_store_B <- c(2, 5, 6, 4, 8, 3)
+quantity_store_C <- c(4, 5, 2, 1, 8, 3)
+store_quantity <- rbind(quantity_store_A, quantity_store_B, quantity_store_C)
+print(store_quantity)
+
+quantity <- list(quantity_store_A, store_quantity)
+quantity
+
+# Using named lists
+basket_details <- list(store = 'Store A', product = c('apple', 'cookie'))
+basket_details
+basket_details[[1]]
+basket_details[['product']]
+basket_details$product
+basket_details[[2]][[2]] #cookie
